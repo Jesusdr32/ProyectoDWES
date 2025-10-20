53a55d738b5775c620eb14e379e00ab469607b09
Jesusdr32 <jesus.debenrodriguez@gmail.com>
2025-10-20 18:29:50 +0200

    20/10/2025 modificación en clase de DWES

--- aplicacion/cabecera.php
+++ aplicacion/cabecera.php
@@ -1,14 +1,12 @@
 <?php
-session_start();
-
 //Incluir la clase CalendarDataAccess
-require_once __DIR__ . '../DesarrolloAplicacionPHP/data-access/CalendarDataAccess.php';
+require_once __DIR__ . '/../DesarrolloAplicacionPHP/data-access/CalendarDataAccess.php';
 
 //Ruta al archivo de base de datos SQLite
-$dbFile = __DIR__ . '../DesarrolloAplicacionPHP/data-access/calendar.db';
+$dbFile = __DIR__ . '/../DesarrolloAplicacionPHP/data-access/calendar.db';
 
 //Ruta a la página logout.php
-$logout = __DIR__ . '../aplicacion/logout.php';
+$logout = '../aplicacion/logout.php';
 
 //Ruta a la página change-password.php
 $change_password = '#!';
@@ -42,7 +40,6 @@ if (isset($_SESSION['user_id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cabecera</title> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
 </head>
@@ -54,7 +51,7 @@ if (isset($_SESSION['user_id'])) {
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <?php if ($name): ?>
                     <li class="nav-item">
-                        <span class="navbar-text"><i class="fa-solid fa-user"></i><?= htmlspecialchars($name . ' ' . $last_name) ?></span>
+                        <span class="navbar-text"><i class="fa-solid fa-user"></i><?= $name . ' ' . $last_name ?></span>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="<?= $user_profile ?>">Perfil de usuario</a>
@@ -67,7 +64,7 @@ if (isset($_SESSION['user_id'])) {
                     </li>
                 <?php else: ?>
                     <li class="nav-item">
-                        <span class="navbar-text">Usuario no identificado</span>
+                        <span class="navbar-brand">Usuario no identificado</span>
                     </li>
                 <?php endif; ?>
             </ul>
--- aplicacion/events.php
+++ aplicacion/events.php
@@ -2,13 +2,13 @@
 session_start();
 
 //Incluir la clase cabecera.php
-include __DIR__ . '../aplicacion/cabecera.php';
+include __DIR__ . '/../aplicacion/cabecera.php';
 
 //Incluir la clase CalendarDataAccess
-require_once __DIR__ . '../DesarrolloAplicacionPHP/data-access/CalendarDataAccess.php';
+require_once __DIR__ . '/../DesarrolloAplicacionPHP/data-access/CalendarDataAccess.php';
 
 //Ruta al archivo de base de datos SQLite
-$dbFile = __DIR__ . '../DesarrolloAplicacionPHP/data-access/calendar.db';
+$dbFile = __DIR__ . '/../DesarrolloAplicacionPHP/data-access/calendar.db';
 
 //Crear instancia de acceso a datos
 $dataAccess = new CalendarDataAccess($dbFile);
@@ -23,6 +23,7 @@ if (!isset($_SESSION['user_id'])) {
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -30,12 +31,25 @@ if (!isset($_SESSION['user_id'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
+
 <body>
     <div class="container d-flex mt-5 justify-content-center align-items-center vh-100 p-5" style="font-size: 1.2rem;">
         <div>
             <h2 class="mb-4 text-center">Eventos</h2>
-
+            <div class="container">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Titulo</th>
+                            <th>Descripcion</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
         </div>
     </div>
 </body>
+
 </html>
