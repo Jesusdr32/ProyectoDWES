05575899f21e1ac42bccdf13505a6a0da723012e
Jesusdr32 <jesus.debenrodriguez@gmail.com>
2025-12-03 19:34:14 +0100

    Adición de la carpeta de IntelliJ para el proyecto de Spring

---
+++ IntelliJ/a06-actividadSpringFundamental/src/main/java/dwes/a06actividadspringfundamental/A06ActividadSpringFundamentalApplication.java
@@ -0,0 +1,13 @@
+package dwes.a06actividadspringfundamental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A06ActividadSpringFundamentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A06ActividadSpringFundamentalApplication.class, args);
+    }
+
+}
---
+++ IntelliJ/a06-actividadSpringFundamental/src/test/java/dwes/a06actividadspringfundamental/A06ActividadSpringFundamentalApplicationTests.java
@@ -0,0 +1,13 @@
+package dwes.a06actividadspringfundamental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A06ActividadSpringFundamentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
