6ce3d77d59114923a3d68259ba3d520e05ca3ff1
Jesusdr32 <jesus.debenrodriguez@gmail.com>
2025-12-05 19:32:33 +0100

    Modificcion del 4/12/25

---
+++ IntelliJ/shop-eah-jdr/shop-eah-jdr-common/src/main/java/es/iesclaradelrey/da2d1e/shopeahjdr/common/entities/Category.java
@@ -0,0 +1,9 @@
+package es.iesclaradelrey.da2d1e.shopeahjdr.common.entities;
+
+public class Category {
+    private Long id;
+    private String name;
+    private String description;
+    //private image;
+
+}
---
+++ IntelliJ/shop-eah-jdr/shop-eah-jdr-common/src/main/java/es/iesclaradelrey/da2d1e/shopeahjdr/common/repositories/CategoryRepository.java
@@ -0,0 +1,4 @@
+package es.iesclaradelrey.da2d1e.shopeahjdr.common.repositories;
+
+public class CategoryRepository  {
+}
