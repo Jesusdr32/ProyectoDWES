1cd2909ac47616848b1527e1ef4273207c23979d
Jesusdr32 <jesus.debenrodriguez@gmail.com>
2026-01-12 17:08:14 +0100

    Modificaciones antes de las vacaciones de navidad

--- IntelliJ/shop-eah-jdr/shop-eah-jdr-web/src/main/java/es/iesclaradelrey/da2d1e/shopeahjdr/web/controllers/HomeController.java
+++ IntelliJ/shop-eah-jdr/shop-eah-jdr-web/src/main/java/es/iesclaradelrey/da2d1e/shopeahjdr/web/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package es.iesclaradelrey.da2d1e.shopeahjdr.web.controllers;
 
 
+import es.iesclaradelrey.da2d1e.shopeahjdr.common.services.CategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,9 +9,16 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 
+    private final CategoryService categoryService;
+
+    public HomeController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
     @GetMapping("/")
     public ModelAndView index(){
         ModelAndView mv = new ModelAndView("index");
+        mv.addObject("categories", categoryService.findAll());
         return mv;
     }
 
