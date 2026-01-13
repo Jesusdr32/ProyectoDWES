49c62b88eb352e65353d1227391aaff01867c0b7
Jesus <jesus.debenrodriguez@gmail.com>
2026-01-13 20:53:09 +0100

    Creacion de condiciones de uso

--- IntelliJ/shop-eah-jdr/shop-eah-jdr-web/src/main/java/es/iesclaradelrey/da2d1e/shopeahjdr/web/controllers/WebConfiguration.java
+++ IntelliJ/shop-eah-jdr/shop-eah-jdr-web/src/main/java/es/iesclaradelrey/da2d1e/shopeahjdr/web/controllers/WebConfiguration.java
@@ -1,4 +1,14 @@
 package es.iesclaradelrey.da2d1e.shopeahjdr.web.controllers;
 
-public class WebConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/terms").setViewName("terms");
+    }
 }
